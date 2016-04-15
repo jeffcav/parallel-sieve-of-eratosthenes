acc8163bac15de5a4481778553ed54dbb2df56a8
Jefferson Cavalcante <recife.rodrigo@gmail.com>
2016-04-14 21:01:10 -0300

    Implementing the Sieve of Eratosthenes (SoE)
    Implementation of serialized and multi-threaded SoE

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 Be my guest exploring parallelized version of known algorithms.
+
+Subprojects:
+
+1. Sieve of Eratosthenes [sieve/]
+This implementation follows the guidelines proposed in http://www.shodor.org/petascale/materials/UPModules/sieveOfEratosthenes/
---
+++ sieve/sieve.serial.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX_PRIMOS 1000000
+#define true 1
+#define false 0
+
+char *primos;
+long int max;
+
+int crivo(int n)
+{
+	int i, j, max;
+	if (n<=1)
+		return -1;
+	
+	max = (int)sqrt((double)n);
+	for (i=2; i < max; i++) {
+		if (primos[i] == true) {
+			for (j = i * i; j < n; j += i) {
+				primos[j] = false;
+			}
+		}
+	}
+	
+	return 0;
+}
+
+int main(int argc, char *argv[])
+{
+	int i, cycles = 1;
+	
+	switch (argc) {
+		case 3:
+			cycles = atoi(argv[2]);
+			if (cycles < 0 || cycles > 1000)
+				exit(1);
+		case 2:
+			max = atoi(argv[1]);
+			if (max < 0 || max > 100000000)
+				exit(1);
+			
+			primos = calloc(max, sizeof(char));
+			break;
+			
+		default:
+			exit(1);
+	}
+	
+	
+	do{
+		for(i=0; i < max; i++){
+			primos[i] = true;
+		}
+		
+		crivo(max);
+		
+		/*
+		for(i=2; i < max; i++){
+			if (primos[i] == true)
+				printf("%d ", i);
+		}
+		printf("\n");
+		*/
+	} while (cycles-- > 1);
+	
+	free(primos);
+	return 0;
+}
+	
+	
